1c90faeb331448d9dd21057685a15cb12db8b415
Pedro Miguel Ferreira Ribeiro <56833458+PedromfRibeiro@users.noreply.github.com>
2024-01-21 23:58:45 +0000

    Dev/application layer (#11)
    * Application Layer was created, DTO Exception, Helper (filter and pagination), Interfaces and Services (TokenService)

---
+++ Application/Helper/PagedList.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Application.Helper;
+
+public class PagedList<T> : List<T>
+{
+	public PagedList(int currentPage, int totalPages, int pageSize, int totalCount)
+	{
+		this.CurrentPage = currentPage;
+		this.TotalPages = totalPages;
+		this.PageSize = pageSize;
+		this.TotalCount = totalCount;
+	}
+	public int CurrentPage { get; set; }
+	public int TotalPages { get; set; }
+	public int PageSize { get; set; }
+	public int TotalCount { get; set; }
+	public PagedList(IEnumerable<T> items, int count, int pageNumber, int pageSize)
+	{
+		CurrentPage = pageNumber;
+		TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+		PageSize = pageSize;
+		TotalCount = count;
+		AddRange(items);
+	}
+	public static async Task<PagedList<T>> CreateAsync(IQueryable<T> source, int pageNumber, int pageSize, List<FilterOptions> filterOptions)
+	{
+		if (filterOptions != null)
+		{
+			foreach (var options in filterOptions)
+			{
+				source = source.Where(FilterExpression.BuildPredicate<T>(options.field, options.operators, options.value));
+			}
+		}
+		var count = await source.CountAsync();
+		var items = await source.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToListAsync();
+		return new PagedList<T>(items, count, pageNumber, pageSize);
+	}
+}
