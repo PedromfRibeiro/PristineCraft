0f577e46e2bb849e44e977dc2099bb27f27aa46b
Pedro Miguel Ferreira Ribeiro <56833458+PedromfRibeiro@users.noreply.github.com>
2024-01-30 22:14:34 +0000

    Persistence Layer (#12)

---
+++ Application/DTO/User/RegisterDto.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Application.DTO.User;
+
+public class RegisterRequestDto
+{
+	[Required]
+	public string Name { get; set; }
+	public string Contact { get; set; }
+	public string PhoneNumber { get; set; }
+	public string Observations { get; set; }
+
+
+	//IdentityUser Fields
+	[Required]
+	public string UserName { get; set; }
+	[Required]
+	public string Email { get; set; }
+	[Required]
+	[StringLength(20, MinimumLength = 4)]
+	public string Password { get; set; }
+}
+public class RegisterResponseDTO
+{
+	public string UserName { get; set; }
+	public string Email { get; set; }
+}
---
+++ Application/Interfaces/IAccountRepository.cs
@@ -0,0 +1,28 @@
+﻿using Application.DTO.User;
+using Application.Helper;
+using Domain.Entities.User;
+using Microsoft.AspNetCore.Identity;
+using OneOf;
+using OneOf.Types;
+
+namespace Application.Interfaces;
+
+public interface IAccountRepository
+{
+	Task<OneOf<User, NotFound>> Fetch(string email);
+	Task<OneOf<PagedList<User>, NotFound>> Fetch(FilterParams filterParams);
+	Task<bool> Update(User request);
+	Task<bool> Delete(User request);
+	Task<OneOf<(SignInResult, User), Error<string>>> AccountLogin(LoginRequestDto userLogin);
+	Task AccountLogOut();
+	Task<OneOf<IdentityResult, Error<string>>> AccountRole_Add(string email, string role);
+	Task<OneOf<IdentityResult, Error<string>>> AccountRole_Remove(string email, string role);
+	Task<OneOf<string, Error<string>>> AccountPhone_CreateToken(string Email, string phoneNumber);
+	Task<OneOf<bool, Error<string>>> AccountPhone_Change(string Email, string token, string newPhoneNumber);
+	Task<OneOf<bool, Error<string>>> AccountPhone_Confirmed(string Email);
+	Task<OneOf<bool, Error<string>>> AccountPassword_Change(string Email, string newPassword, string token);
+	Task<OneOf<string, Error<string>>> AccountPassword_CreateToken(string Email);
+	Task<OneOf<TfaSetupDto, NotFound>> TwoFactorAuthSetup(string email);
+	Task<OneOf<TfaSetupDto, NotFound, Error<string>>> PostTfaSetup(TfaSetupDto tfaModel);
+
+}
