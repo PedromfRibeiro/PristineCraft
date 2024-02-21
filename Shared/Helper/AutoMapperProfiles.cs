e823bd29d0b81bd877f6b06730535761be891712
Pedro Miguel Ferreira Ribeiro <prs.p.ribeiro@gmail.com>
2024-02-21 01:55:47 +0000

    Added Shared project li brary and AutoMapper Profile support

---
+++ Shared/Helper/AutoMapperProfiles.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Domain.Entities.User;
+using Application.DTO.User;
+
+namespace Shared.Helper;
+public class AutoMapperProfiles : Profile
+{
+	public AutoMapperProfiles()
+	{
+		CreateMap<User, LoginRequestDto>().ReverseMap();
+		CreateMap<User, LoginResponseDto>().ReverseMap();
+		CreateMap<User, RegisterRequestDto>().ReverseMap();
+		CreateMap<User, RegisterResponseDTO>().ReverseMap();
+		CreateMap<User, TfaSetupDto>().ReverseMap();
+	}
+}
