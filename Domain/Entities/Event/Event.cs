c5274dee09db5f3e8d1f0f9340c82ed570b25c8a
Pedro Miguel Ferreira Ribeiro <56833458+PedromfRibeiro@users.noreply.github.com>
2024-04-04 01:06:04 +0100

    Event Class

---
+++ Domain/Entities/Event/Event.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Entities.Event;
+
+public class Event
+{
+	[Key]
+	public int Id { get; set; }
+	public string? Description { get; set; }
+	public decimal Amount { get; set; }
+	public DateTime Date { get; set; }
+	public DateTime CreationDate { get; set; } = DateTime.UtcNow;
+	public DateTime ModifiedDate { get; set; } = DateTime.UtcNow;
+	public EventCategory? Category { get; set; }
+	public EventSubCategory? SubCategory { get; set; }
+	public required User Owner { get; set; }
+	public Group? Group { get; set; }
+	public ICollection<byte[]>? Attachments { get; set; }
+}
---
+++ Domain/Entities/Event/EventSubCategory.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Entities.Event;
+
+public class EventSubCategory
+{
+	[Key]
+	public int Id { get; set; }
+	public required string Name { get; set; }
+	public EventCategory? Category { get; set; }
+}
