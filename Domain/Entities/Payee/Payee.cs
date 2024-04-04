8485085f50e0935d193d08555dfb2544d946008a
Pedro Miguel Ferreira Ribeiro <56833458+PedromfRibeiro@users.noreply.github.com>
2024-04-04 01:06:27 +0100

    Payee entities

---
+++ Domain/Entities/Payee/Payee.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Entities;
+
+public class Payee
+{
+	[Key]
+	public int Id { get; set; }
+
+	public required string Name { get; set; }
+	public string? Description { get; set; }
+	public required PayeeCategory Category { get; set; }
+}
