3c19250fceace13ac9f9a1271cbd36650a573230
Pedro Miguel Ferreira Ribeiro <56833458+PedromfRibeiro@users.noreply.github.com>
2024-04-18 15:01:03 +0100

    Start Blazor project

---
+++ WebClient/DependencyInjection.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.FluentUI.AspNetCore.Components;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Builder;
+using WebClient;
+
+namespace WebClient.Presentation;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddPresentation(this IServiceCollection services)
+    {
+        services.AddRazorComponents().AddInteractiveServerComponents();
+        services.AddFluentUIComponents();
+        return services;
+    }
+
+    public static WebApplication UsePresentation(this WebApplication application)
+    {
+        application.MapRazorComponents<App>().AddInteractiveServerRenderMode();
+        return application;
+    }
+}
