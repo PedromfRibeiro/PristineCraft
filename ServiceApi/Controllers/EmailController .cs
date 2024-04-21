cbc7180934b06969de6614eaf01ba6825bb2f79a
Pedro Miguel Ferreira Ribeiro <56833458+PedromfRibeiro@users.noreply.github.com>
2024-04-21 19:06:53 +0100

    Api's Changes

--- ServiceApi/Controllers/EmailController .cs
+++ ServiceApi/Controllers/EmailController .cs
@@ -1,25 +1,25 @@
-﻿using Domain.Entities.Services;
-using Infrastructure.Interfaces;
+﻿using Infrastructure.Interfaces;
 using Microsoft.AspNetCore.Mvc;
-using Shared.Controllers;
+using Infrastructure.Controller;
+using PristineCraft.Domain.Entities.Services;
 
 namespace ServiceApi.Controllers;
 
 public class EmailController : BaseApiController
 {
-	private readonly IEmailService _emailService;
+    private readonly IEmailService _emailService;
 
-	public EmailController(IEmailService emailService)
-	{
-		_emailService = emailService
-			?? throw new ArgumentNullException(nameof(emailService));
-	}
+    public EmailController(IEmailService emailService)
+    {
+        _emailService = emailService
+            ?? throw new ArgumentNullException(nameof(emailService));
+    }
 
-	[HttpGet("singleemail")]
-	public async Task<IActionResult> SendSingleEmail()
-	{
-		EmailMessageModel emailMetadata = new("john.doe@gmail.com", "FluentEmail Test email", "This is a test email from FluentEmail.");
-		await _emailService.Send(emailMetadata);
-		return Ok();
-	}
+    [HttpGet("singleemail")]
+    public async Task<IActionResult> SendSingleEmail()
+    {
+        EmailMessageModel emailMetadata = new("john.doe@gmail.com", "FluentEmail Test email", "This is a test email from FluentEmail.");
+        await _emailService.Send(emailMetadata);
+        return Ok();
+    }
 }
