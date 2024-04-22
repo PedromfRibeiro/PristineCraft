b97add1049211d044d9e5f10724458101f81b1cc
Pedro Miguel Ferreira Ribeiro <56833458+PedromfRibeiro@users.noreply.github.com>
2024-04-22 13:58:01 +0100

    Seeds changes

--- Infrastructure/Seed/Seeds.cs
+++ Infrastructure/Seed/Seeds.cs
@@ -4,6 +4,9 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using PristineCraft.Domain.Entities.Payee;
+using PristineCraft.Domain.Entities;
 
 namespace Infrastructure.Helper;
 
@@ -19,18 +22,18 @@ public static class Seeds
             {
                 _context.Database.EnsureDeleted();
                 _context.Database.Migrate();
+                await CreateCategories(_context);
+                await CreateBankAccount(_context);
                 await UserSeedAsync(userManager, _context, roleManager);
-                Seeder.CreateCategories(_context);
-                Seeder.CreateBankAccount(_context);
             }
             catch
             {
-                throw new Exception("An error occurred durring migration");
+                Console.WriteLine("An error occurred durring migration");
             }
         }
     }
 
-    public static async Task UserSeedAsync(UserManager<AppUser> userManager, DataContext _context, RoleManager<UserRole> roleManager)
+    private static async Task UserSeedAsync(UserManager<AppUser> userManager, DataContext _context, RoleManager<UserRole> roleManager)
     {
         try
         {
@@ -44,5 +47,46 @@ public static class Seeds
         {
             throw new Exception(ex.ToString());
         }
+    }
+
+    private static async Task CreateCategories(DataContext _context)
+    {
+        var jsonData = File.ReadAllText("../Infrastructure/Seed/Resources/payee.json");
+        var storeCategories = JsonConvert.DeserializeObject<Dictionary<string, List<Payee>>>(jsonData);
+        if (storeCategories == null)
+            return;
+
+        foreach (var item in storeCategories.Keys)
+        {
+            List<Payee> ListPayee = storeCategories[item];
+            var category = _context.DbPayeeCategory.Where(x => x.Name == item).FirstOrDefault();
+            if (category == null)
+            {
+                category = new PayeeCategory() { Name = item };
+            }
+            foreach (Payee payee in ListPayee)
+            {
+                payee.Category = category;
+            }
+            await _context.DbPayee.AddRangeAsync(ListPayee);
+        }
+        _context.SaveChanges();
+    }
+
+    private static async Task CreateBankAccount(DataContext _context)
+    {
+        var jsonData = File.ReadAllText("../Infrastructure/Seed/Resources/BankAccount.json");
+        var items = JsonConvert.DeserializeObject<List<BankAccount>>(jsonData);
+        if (items == null)
+            return;
+
+        foreach (var item in items)
+        {
+            Random rand = new Random();
+            int toSkip = rand.Next(0, _context.DbUser.Count());
+            item.Proprietary = _context.DbUser.OrderBy(r => r.Id).Skip(toSkip).Take(1).FirstOrDefault();
+            await _context.DbBankAccount.AddAsync(item);
+        }
+        _context.SaveChanges();
     }
 }
